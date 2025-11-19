6e4db06aa95b222a7f4a0de712d80b5e8ad5c0c1
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2025-11-19 10:39:04 -0300

    Adicionando cura, esquiva na superclasse

--- src/Lutador.java
+++ src/Lutador.java
@@ -1,10 +1,10 @@
-abstract public class Lutador {
+public abstract class Lutador {
     String nome;
     int vida;
     int energia;
     int forca;
 
-    public Lutador (String nome, int vida, int energia, int forca){
+    public Lutador(String nome, int vida, int energia, int forca) {
         this.nome = nome;
         this.vida = vida;
         this.energia = energia;
@@ -14,35 +14,29 @@ abstract public class Lutador {
     public abstract void atacar(Lutador oponente);
     public abstract void ataqueEspecial(Lutador oponente);
 
-    public void defender (){
-        if (energia < 20){
+    public void defender() {
+        if (energia < 20) {
             energia += 20;
-            System.out.println("Recuperando energia.");
-        } else{
+            System.out.println(nome + " recuperou 20 de energia.");
+        } else {
             int reducao = forca;
-            System.out.println("Defendeu! Redução: " + reducao);
+            System.out.println(nome + " defendeu! Redução de dano: " + reducao);
         }
     }
 
-    public void mostrarStatus(){
-        System.out.println("Nome: " + this.nome);
-        System.out.println("Vida: " + this.vida);
-        System.out.println("Energia: " + this.energia);
+    public void mostrarStatus() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Vida: " + vida);
+        System.out.println("Energia: " + energia);
     }
 
-    public boolean estaVivo(){
-        if (this.vida > 0){
-            System.out.println("Está vivo!");
-            return true;
-        } else{
-            System.out.println("Está morto!");
-            return false;
-        }
+    public boolean estaVivo() {
+        return vida > 0;
     }
 
     public void curar() {
         this.vida += 20;
-        if (this.vida > 100) this.vida = 100;  // limita vida
+        if (this.vida > 100) this.vida = 100;
         System.out.println(nome + " se curou 20 pontos.");
     }
 
@@ -54,12 +48,10 @@ abstract public class Lutador {
 
     public boolean tentarDesviar() {
         double chance = Math.random();
-
-        if (chance < 0.20) { // 20% de chance
+        if (chance < 0.20) {
             System.out.println(nome + " desviou do ataque!");
             return true;
         }
         return false;
     }
-
 }
