265c507d527e33e9f4d1bc98de69244c08b01e45
Guilherme Caçuci Ladislau <0001155878@senaimgaluno.com.br>
2025-11-19 10:38:17 -0300

    Alterando a subclasse lutador leve

--- src/LutadorLeve.java
+++ src/LutadorLeve.java
@@ -1,10 +1,10 @@
-public class LutadorLeve extends Lutador{
+public class LutadorLeve extends Lutador {
     public LutadorLeve(String nome) {
         super(nome, 100, 100, 10);
     }
 
     @Override
-    public void atacar(Lutador oponente){
+    public void atacar(Lutador oponente) {
         if (oponente.tentarDesviar()) return;
 
         int dano = forca + 5;
@@ -14,11 +14,11 @@ public class LutadorLeve extends Lutador{
     }
 
     @Override
-    public void ataqueEspecial (Lutador oponente){
+    public void ataqueEspecial(Lutador oponente) {
         int gasto = 15;
-        this.energia -= gasto;
+        energia -= gasto;
         oponente.vida -= (forca + gasto);
-        System.out.println(nome + " usou ataque especial removendo " + gasto + " de sua energia!");
-        System.out.println(oponente.nome + " perdeu " + gasto + " de vida!");
+        System.out.println(nome + " usou ataque especial!");
+        System.out.println(oponente.nome + " perdeu " + (forca + gasto) + " de vida!");
     }
 }
